ebaf78f625370af28a66d8b567633cd3a26000d2
Maksim "s1Sharp" Tsarkov <s1sharp.it@gmail.com>
2024-03-24 22:28:14 +0300

    send command process working

--- app/db/client/main.cpp
+++ app/db/client/main.cpp
@@ -1,17 +1,45 @@
 #include <cstdlib>
-#include "spdlog/spdlog.h"
+
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+
 #include "defaults.hpp"
 #include "client.hpp"
 
+void set_logger(bool useConsole)
+{
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+
+    // Create a file rotating logger with 1 MB size max and 3 rotated files
+    auto max_size = 1024 * 1024 * 1;
+    auto max_files = 3;
+    std::vector<spdlog::sink_ptr> sinks{};
+    if (useConsole) {
+        sinks.push_back(console_sink);
+    }
+	auto rotate_file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("log/fancy.cli.log", max_size, max_files); // 1 MB, 3 файлов
+	sinks.push_back(rotate_file_sink);
+
+    // Добавим файловый синк в дефолтный логгер
+    spdlog::default_logger_raw()->sinks() = sinks;
+
+    spdlog::default_logger_raw()->flush_on(spdlog::level::info);
+    spdlog::flush_every(std::chrono::seconds(5));
+
+    spdlog::default_logger_raw()->set_level(spdlog::level::debug);
+}
+
 int main()
 {
-	spdlog::default_logger()->set_pattern("[CLIENT] %+");
-	spdlog::default_logger()->set_level(spdlog::level::debug);
+    set_logger(false);
 
 	client_t client{defaults::server::ip, defaults::server::port};
 	if (!client.start())
 	{
 		return EXIT_FAILURE;
 	}
+
 	return EXIT_SUCCESS;
 }
--- app/db/server/main.cpp
+++ app/db/server/main.cpp
@@ -1,12 +1,38 @@
-#include "spdlog/spdlog.h"
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/rotating_file_sink.h>
 
 #include "defaults.hpp"
 #include "server.hpp"
 
+void set_logger(bool useConsole)
+{
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+
+    // Create a file rotating logger with 5 MB size max and 3 rotated files
+    auto max_size = 1024 * 1024 * 5;
+    auto max_files = 3;
+    std::vector<spdlog::sink_ptr> sinks{};
+    if (useConsole) {
+        sinks.push_back(console_sink);
+    }
+    auto rotate_file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("log/fancy.serv.log", max_size, max_files); // 5 MB, 3 файлов
+    sinks.push_back(rotate_file_sink);
+
+    // Добавим файловый синк в дефолтный логгер
+    spdlog::default_logger_raw()->sinks() = sinks;
+
+    spdlog::default_logger_raw()->flush_on(spdlog::level::info);
+    spdlog::flush_every(std::chrono::seconds(5));
+
+    spdlog::default_logger_raw()->set_level(spdlog::level::debug);
+}
+
+
 int main()
 {
-	spdlog::default_logger()->set_pattern("[SERVER] %+");
-	spdlog::default_logger()->set_level(spdlog::level::debug);
+    set_logger(true);
 
 	server_t server{defaults::server::ip, defaults::server::port, defaults::server::threads};
 	if (!server.start())
